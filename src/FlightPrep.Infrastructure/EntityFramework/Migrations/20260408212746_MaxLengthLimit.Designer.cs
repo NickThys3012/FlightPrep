e94c236ab4f538a9ee1f162cccab951b41bda144
NickThys3012 <nick.thys@vlaanderen.be>
2026-04-08 23:29:37 +0200

    more fixes

--- src/FlightPrep.Infrastructure/EntityFramework/Migrations/20260408212746_MaxLengthLimit.Designer.cs
+++ src/FlightPrep.Infrastructure/EntityFramework/Migrations/20260408212746_MaxLengthLimit.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using FlightPrep.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace FlightPrep.Data.Migrations
+namespace FlightPrep.Infrastructure.EntityFramework.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260408212746_MaxLengthLimit")]
+    partial class MaxLengthLimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
